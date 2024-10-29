068272d0799ab595a6b20bcc98027d27aabdfddd
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-10-30 01:10:27 +0530

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import logo from '../assets/logo.png';
+
+function Header() {
+  return (
+    <header className="mt-8 flex flex-col items-center gap-y-4">
+      <img
+        src={logo}
+        className="w-20 h-20 object-contain drop-shadow-lg"
+        alt="Stylized Globe"
+      />
+      <p className="font-bold text-6xl tracking-[1rem] uppercase">
+        Placepicker
+      </p>
+      <p className="w-[40ch] text-lg text-center">
+        Create your personal collection of places you would like to visit or you
+        have visited.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
