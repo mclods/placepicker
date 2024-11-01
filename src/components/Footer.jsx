35f1b73ed4a333f4c48e31ba57ed64a2148ee1f6
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-11-01 20:54:23 +0530

    Added cypress coverage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full px-40 py-12 bg-slate-100">
-      <p>&#169; {currentYear} Placepicker</p>
+    <footer
+      className="w-full px-40 py-12 bg-slate-100"
+      data-testid="footer-section"
+    >
+      <p data-testid="company-copyright-text">
+        &#169; {currentYear} Placepicker
+      </p>
     </footer>
   );
 }
