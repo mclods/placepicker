457fda7eae6bb1b87ebfff690a6fdd4b0ef0a55f
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-11-02 19:33:16 +0530

    Added cypress coverage

--- cypress/e2e/e2e-test.cy.js
+++ cypress/e2e/e2e-test.cy.js
@@ -59,4 +59,83 @@ describe('Placepicker Tests', () => {
       `© ${currentYear} Placepicker`
     );
   });
+
+  it('Pick places to visit', () => {
+    // Select all available places
+    cy.getTestId('places-section')
+      .eq(1)
+      .find('[data-testid="place-list-item"]')
+      .each((placeItem) => {
+        cy.wrap(placeItem).click();
+      });
+
+    // Assert all picked places
+    cy.getTestId('places-section')
+      .eq(0)
+      .then((place) => {
+        cy.wrap(place)
+          .find('[data-testid="places-section-title"]')
+          .should('have.text', "I'd like to visit ...");
+
+        cy.wrap(place)
+          .find('[data-testid="places-fallback-text"]')
+          .should('not.exist');
+
+        cy.wrap(place)
+          .find('[data-testid="place-list-item"]')
+          .each((placeItem, index) => {
+            cy.wrap(placeItem)
+              .find('img')
+              .should('have.attr', 'alt', fixture[index].image.alt);
+            cy.wrap(placeItem)
+              .find('p')
+              .should('have.text', fixture[index].title);
+          });
+      });
+  });
+
+  it('Delete all picked places', () => {
+    // Select a picked place to delete
+    cy.getTestId('places-section')
+      .eq(0)
+      .find('[data-testid="place-list-item"]')
+      .eq(0)
+      .click();
+
+    // Assert delete confirmation dialog and press No
+    cy.getTestId('dialog-title').should('have.text', 'Delete Place');
+    cy.getTestId('dialog-message').should(
+      'have.text',
+      'Are you sure you want to delete this place ?'
+    );
+    cy.getTestId('yes-btn').should('have.text', 'Yes');
+    cy.getTestId('no-btn').should('have.text', 'No').click();
+    cy.getTestId('dialog-container').should('not.be.visible');
+
+    // Select all picked places for deletion
+    cy.getTestId('places-section')
+      .eq(0)
+      .find('[data-testid="place-list-item"]')
+      .each((placeItem) => {
+        cy.wrap(placeItem).click();
+
+        // Confirm deletion by pressing Yes
+        cy.getTestId('yes-btn').click();
+      });
+
+    // Assert no picked places exist
+    cy.getTestId('places-section')
+      .eq(0)
+      .then((place) => {
+        cy.wrap(place)
+          .find('[data-testid="places-section-title"]')
+          .should('have.text', "I'd like to visit ...");
+        cy.wrap(place)
+          .find('[data-testid="places-fallback-text"]')
+          .should(
+            'have.text',
+            'Select the places you would like to visit below.'
+          );
+      });
+  });
 });
--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -24,9 +24,14 @@ const Modal = forwardRef(function Modal(
     <dialog
       ref={dialogRef}
       className="w-[30vw] p-4 rounded-md backdrop:bg-stone-900/90 modal"
+      data-testid="dialog-container"
     >
-      <p className="mb-2 font-bold text-2xl">{title}</p>
-      <p className="mb-2">{message}</p>
+      <p className="mb-2 font-bold text-2xl" data-testid="dialog-title">
+        {title}
+      </p>
+      <p className="mb-2" data-testid="dialog-message">
+        {message}
+      </p>
       <form method="dialog">{buttons}</form>
     </dialog>,
     document.getElementById('modal')
